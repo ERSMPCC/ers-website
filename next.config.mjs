92cbccf10fceda500fe9d0fbfb003c602ae07d5d
ANACONDAJUTS <59391646+ANACONDAJUTS95@users.noreply.github.com>
2025-07-22 14:17:08 +0800

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
+const repo = 'ers-website'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 const nextConfig = {
+  output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    unoptimized: true,
+  },
   // Enable path mapping
   experimental: {
     // This ensures the @ alias works properly
